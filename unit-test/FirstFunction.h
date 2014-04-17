500441b95a81d19ecce36ee31e4e0effe125fad1
Alex <alex.kovalskiy@icloud.com>
2014-04-17 21:08:08 +0300

    simple test

---
+++ unit-test/FirstFunction.h
@@ -0,0 +1,15 @@
+//
+//  FirstFunction.h
+//  unit-test
+//
+//  Created by Alex on 4/17/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FirstFunction : NSObject
+
+- (int)setX:(int)x setY:(int)y;
+
+@end
---
+++ unit-test/FirstFunction.m
@@ -0,0 +1,18 @@
+//
+//  FirstFunction.m
+//  unit-test
+//
+//  Created by Alex on 4/17/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import "FirstFunction.h"
+
+@implementation FirstFunction
+
+/** return (x * y). */
+- (int)setX:(int)x setY:(int)y {
+    return x * y;
+}
+
+@end
