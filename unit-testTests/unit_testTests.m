775009ef80ac33fb33d1e5395c8a40b66aad476e
Alex <alex.kovalskiy@icloud.com>
2014-04-17 21:00:11 +0300

    re group

---
+++ unit-testTests/unit_testTests.m
@@ -0,0 +1,34 @@
+//
+//  unit_testTests.m
+//  unit-testTests
+//
+//  Created by Alex on 4/17/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface unit_testTests : XCTestCase
+
+@end
+
+@implementation unit_testTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
